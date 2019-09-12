ad8b436a7bfe0f6f7b8328a88cbe1f7227819c70
qianqianwang-bit <54992496+qianqianwang-bit@users.noreply.github.com>
2019-09-12 09:43:53 +0800

    Add files via upload

---
+++ EshopDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+
+namespace Eshop.Models
+{
+    public class EshopDBContext:DbContext
+    {
+        public DbSet<Product> Product { get; set; }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Eshop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
